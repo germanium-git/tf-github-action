cc0e5a7f08eb054a7b2c547bcb323f4213c4d5e6
germanium-git <germanium-git@gmx.com>
2023-02-21 18:29:16 +0100

    namespace fixed

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,7 +1,12 @@
+data "oci_objectstorage_namespace" "my_namespace" {
+
+    #Optional
+    compartment_id = var.COMPARTMENT_OCID
+}
+
 resource "oci_objectstorage_bucket" "test_bucket" {
     #Required
     compartment_id = var.COMPARTMENT_OCID
     name = "2023testabc"
-    namespace = "mynamespace"
-
+    namespace = data.oci_objectstorage_namespace.my_namespace.name
 }
